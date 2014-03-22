3f1db4c235a1a5da3d3fe031807ac398a1a66d5f
halfnelson <dpershouse@gmail.com>
2014-03-23 00:43:35 +1000

    still nutting out the app structure. use streams of objects from anydb instead of just arrays of values.

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes');
-var users = require('./routes/user');
+
+var editor = require('./routes/edit');
 
 var app = express();
 var fs = require('fs');
@@ -39,8 +39,14 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(app.router);
 
-app.get('/', routes.index);
-app.get('/users', users.list);
+//resolve our slugs and versions
+app.param('slug')
+
+app.get('/:slug/:version', editor.show);
+app.get('/:slug', editor.show);
+app.post('/:slug/:version', editor.show);
+app.post('/:slug', editor.save);
+
 
 /// catch 404 and forwarding to error handler
 app.use(function(req, res, next) {
